b6629c85407675c668ee870785ac73f1c639e3ff
Elad Mines <mineselad@gmail.com>
2024-03-08 16:54:57 +0200

    Remove the printing of users' details

--- app/register/queries.py
+++ app/register/queries.py
@@ -4,12 +4,6 @@ from dbConnections import openConnection, closeConnection
 def generate_insert_query(data):
      con=openConnection()
      curs=con.cursor()
-     print("type:",type( data['lastName']))
-     print("type:",type( data['lastName']))
-     print("type:",type( data['email']))
-     print("type:",type(  data['inputPassword']))
-     print("type:",type(  data['role']))
-     print("type:",type(  data['company']))
      insertUser = "INSERT INTO users (firstName, lastName, email, password, role, company) " \
                    "VALUES (%s, %s, %s, %s, %s, %s);"
      values=(data['firstName'], data['lastName'], data['email'],
