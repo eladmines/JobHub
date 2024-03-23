ff7e7f827516df1a1225a0ea1287170c7bb95a28
Elad Mines <mineselad@gmail.com>
2024-03-23 18:04:54 +0200

    change the connection details from lowercase letters to  uppercase letters

--- app/data/constants.py
+++ app/data/constants.py
@@ -1,12 +1,11 @@
 url = "https://jobs.careers.microsoft.com/global/en/search?et=Internship&l=en_us&pg=1&pgSz=20&o=Relevance&flt=true"
-#DATABASE_URL="arn:aws:rds:us-east-1:690292011599:db:database-1"
 CREATE_JOBS_TABLE=("CREATE TABLE IF NOT EXISTS Jobs (jobTitle TEXT,jobLocation TEXT, jobDescription TEXT,jobQualifications TEXT,jobDate TEXT, jobLink TEXT, jobCompany TEXT, image TEXT);")
 INSERT_JOBS = """INSERT INTO jobs (jobTitle,jobLocation, jobDescription,jobQualifications,jobDate, jobLink, jobCompany, image)
 VALUES (%s, %s, %s, %s, %s, %s, %s,%s);"""
-endpoint ="***" 
-database = "***"
-username="***"
-password="***"
-port="***"
+ENDPOINT ="database-1.czkaa4kseikm.us-east-1.rds.amazonaws.com" 
+DATABASE = "postgres"
+USERNAME="jobby"
+PASSWORD="12345678"
+PORT="5432"
 
-DATABASE_URL = f"host='{endpoint}' dbname='{database}' user='{username}' password='{password}' port='{port}'"
+DATABASE_URL = f"host='{ENDPOINT}' dbname='{DATABASE}' user='{USERNAME}' password='{PASSWORD}' port='{PORT}'"
