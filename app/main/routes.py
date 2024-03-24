8dc52e7e99a492d4b0dd57a5904801a735740df1
Elad Mines <mineselad@gmail.com>
2024-03-25 00:24:18 +0200

    remove cookies condition in @main_bp.route(/main)

--- app/main/routes.py
+++ app/main/routes.py
@@ -7,13 +7,6 @@ main_bp = Blueprint("main_bp", __name__ , template_folder='main')
 
 @main_bp.route("/main")
 def index():
-    username = request.cookies.get('username')
-    if(username):
-        print(username)
-        return render_template('index.html') 
-    else:
-      print("no")
-      #return redirect(url_for('\jobs'))
       return render_template('index.html') 
 
 
