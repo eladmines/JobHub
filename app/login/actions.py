ecfe28a0a825b5e3e8eccc3dc6ef9a092704eece
Elad Mines <mineselad@gmail.com>
2024-03-25 22:03:52 +0200

    Remove unused libraries and comments

--- app/login/actions.py
+++ app/login/actions.py
@@ -1,7 +1,5 @@
 from dbConnections import openConnection, closeConnection     
-from login.queries import CHECK_LOGIN_DETAILS,GET_USER_DETAILS
-import requests,json
-
+from login.queries import CHECK_LOGIN_DETAILS
 
 def checkLoginDetails(data):
      con=openConnection()
@@ -10,15 +8,9 @@ def checkLoginDetails(data):
           values=(data['email'],data['password'])
           curs.execute(CHECK_LOGIN_DETAILS,(values[0],values[1]))
           exists = curs.fetchone()[0]
-          print("Exsits" , exists)
           if (exists == True):
-               #res = curs.execute(GET_USER_DETAILS,(values[0],values[1]))
-               #res=curs.fetchone()
-               ##res = json.dumps(res)
-               print("blabla")
                return True
           else:
-               print("ffffffff")
                return False   
      except Exception as e:
           print(f"Error: {e}")
