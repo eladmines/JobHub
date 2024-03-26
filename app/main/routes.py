8b147c04e191cb8d34803f166d23b678daf7cc4b
Elad Mines <mineselad@gmail.com>
2024-03-26 17:10:55 +0200

    Removing unused packages

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, render_template,request, jsonify,json
+from flask import Blueprint, render_template,request, jsonify
 import psycopg2
 from main.actions import getUserData
 
