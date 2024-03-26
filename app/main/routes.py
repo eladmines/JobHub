6214d62eb46af0c99ce0f73d3180bd1603250a63
Elad Mines <mineselad@gmail.com>
2024-03-26 17:10:43 +0200

    Removing unused packages

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,7 +2,7 @@
 from flask import Blueprint, render_template,request, jsonify,json
 import psycopg2
 from main.actions import getUserData
-from main.queries import GET_USER_DETAILS
+
 main_bp = Blueprint("main_bp", __name__ , template_folder='main')
 
 @main_bp.route("/main")
