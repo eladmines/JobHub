e29ea8b4f5fc6f8d4c0af3ba5169f74ca0f1a944
Elad Mines <mineselad@gmail.com>
2024-03-27 21:43:32 +0200

    remove console.log

---
+++ app/templates/js/main/actions.js
@@ -0,0 +1,47 @@
+var details;
+function checkCookies(){
+    let cookies = document.cookie;
+    if(cookies){
+
+    }
+    else{
+        window.location.href = '\\';
+    }
+}
+
+async function getUserData() {
+    let cookies = document.cookie;
+    try {
+        const response = await fetch('/main', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(cookies),
+        });
+        if (response.ok) {
+            const data = await response.json();
+            return data;
+        } else {
+            throw new Error('Something went wrong');
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function fetchData() {
+    var userData = await getUserData();
+    return userData
+}
+
+(async () => {
+    try {
+        details = await fetchData();
+        document.getElementById("name").innerText=details[0]
+    } catch (error) {
+        console.error('Error', error);
+    }
+})().then(() => {
+    
+});
