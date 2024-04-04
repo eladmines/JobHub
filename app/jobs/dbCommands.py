1ee836db448cb9a8d7f67438cc3f58b5fc625867
Elad Mines <mineselad@gmail.com>
2024-04-04 21:41:55 +0300

    create SAVE_JOB query

--- app/jobs/dbCommands.py
+++ app/jobs/dbCommands.py
@@ -1,3 +1,7 @@
 CREATE_JOBS_TABLE=("CREATE TABLE IF NOT EXISTS Jobs (id SERIAL PRIMARY KEY, jobTitle TEXT, jobDescription TEXT, jobCompany TEXT, image TEXT, date TIMESTAMP);")
 
 getAllJobsDBCommand = """SELECT * FROM jobs"""
+
+GET_USER="SELECT * FROM users WHERE email=%s"
+
+SAVE_JOB="UPDATE users SET array_column = array_column || ARRAY[%s] WHERE email  = %s"
