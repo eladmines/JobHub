a8fe174d50572cc08c05faa85127faee695e2af9
Elad Mines <mineselad@gmail.com>
2024-04-04 21:43:11 +0300

    create actions.py + define save job function

---
+++ app/jobs/actions.py
@@ -0,0 +1,14 @@
+from dbConnections import openConnection, closeConnection     
+from jobs.dbCommands import SAVE_JOB
+
+def saveJob(data):
+     con=openConnection()
+     curs=con.cursor()
+     try:
+          print(data[1])
+          curs.execute(SAVE_JOB,(data[1],data[0]))
+          con.commit() 
+     except Exception as e:
+          print(f"Error: {e}")
+          # Rollback changes in case of an error
+          con.rollback() 
