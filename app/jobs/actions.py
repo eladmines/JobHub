c164b7964d4ca123c0dd170154c7a9289d185ca4
Elad Mines <mineselad@gmail.com>
2024-04-06 00:15:44 +0300

    Add getAllJobs function to actions.py

--- app/jobs/actions.py
+++ app/jobs/actions.py
@@ -1,13 +1,31 @@
 from dbConnections import openConnection, closeConnection     
-from jobs.dbCommands import SAVE_JOB
+from jobs.queries import SAVE_JOB
+import json
+from models.job import Job
+from .queries import GET_ALL_JOBS
+def getAllJobs(con,curs):
+    curs.execute(GET_ALL_JOBS)
+    con.commit()
+    rowCounter=0
+    Jobs=[]
+    row = curs.fetchone()
+    while row is not None:
+        job = Job(row[0],row[1],row[2],row[3], row[4],row[5],row[6],row[7])
+        Jobs.append(job)
+        rowCounter = rowCounter + 1
+        row = curs.fetchone()
+    return Jobs
 
-def saveJob(data):
+
+def saveJob(email,data):
      con=openConnection()
      curs=con.cursor()
      try:
-          curs.execute(SAVE_JOB,(data[1],data[0]))
+          stringData = json.dumps(data)
+          print(type(stringData))
+          curs.execute(SAVE_JOB,(stringData,email))
           con.commit() 
      except Exception as e:
           print(f"Error: {e}")
           # Rollback changes in case of an error
-          con.rollback() 
+          con.rollback()
