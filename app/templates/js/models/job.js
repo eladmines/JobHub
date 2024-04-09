8af7db8a81e78b5c02c6d6477150125ed8d77355
Elad Mines <mineselad@gmail.com>
2024-04-09 23:11:40 +0300

    Add js job class

---
+++ app/templates/js/models/job.js
@@ -0,0 +1,13 @@
+
+export class Job{
+    constructor(title,location,description,qualifications,company,image,date,link){
+        this.title=title
+        this.location=location
+        this.description=description
+        this.qualifications = qualifications
+        this.company=company
+        this.image = image
+        this.date = date
+        this.link=link
+    }
+}
