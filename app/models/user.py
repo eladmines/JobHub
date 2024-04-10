df2d800d1d5a6fedb1afdd86f173e45c482c8a0d
Elad Mines <mineselad@gmail.com>
2024-04-10 22:55:15 +0300

    Add user class

---
+++ app/models/user.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self,email,password,firstname,lastname,role,company):
+        self.email=email
+        self.password=password
+        self.firstname=firstname
+        self.lastname=lastname
+        self.role=role
+        self.company=company
