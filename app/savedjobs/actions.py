87ef7049848a19d7c710225bc42e209221040569
Elad Mines <mineselad@gmail.com>
2024-04-16 23:31:23 +0300

    Handle remove saved job

--- app/savedjobs/actions.py
+++ app/savedjobs/actions.py
@@ -12,3 +12,15 @@ def getSavedJobs(data):
           print(f"Error: {e}")
           # Rollback changes in case of an error
           con.rollback() 
+
+def getSavedJobs(data):
+     con=openConnection()
+     curs=con.cursor()
+     try:
+          curs.execute(GET_SAVED_JOBS,(data['sentData'],))
+          data = curs.fetchone()[0]
+          return data
+     except Exception as e:
+          print(f"Error: {e}")
+          # Rollback changes in case of an error
+          con.rollback() 
