edf36463db67ffe7568b00d3c200f44bd92f47d0
Elad Mines <mineselad@gmail.com>
2024-04-19 16:58:47 +0300

    Remove login details

--- app/data/constants.py
+++ app/data/constants.py
@@ -1,11 +1,11 @@
-url = "https://jobs.careers.microsoft.com/global/en/search?et=Internship&l=en_us&pg=1&pgSz=20&o=Relevance&flt=true"
+url = "https://jobs.careers.microsoft.com/global/en/search?l=en_us&pg=1&pgSz=20&o=Relevance&flt=true&ref=cms"
 CREATE_JOBS_TABLE=("CREATE TABLE IF NOT EXISTS Jobs (jobTitle TEXT,jobLocation TEXT, jobDescription TEXT,jobQualifications TEXT,jobDate TEXT, jobLink TEXT, jobCompany TEXT, image TEXT);")
 INSERT_JOBS = """INSERT INTO jobs (jobTitle,jobLocation, jobDescription,jobQualifications,jobDate, jobLink, jobCompany, image)
 VALUES (%s, %s, %s, %s, %s, %s, %s,%s);"""
-ENDPOINT ="jobby.czkaa4kseikm.us-east-1.rds.amazonaws.com" 
-DATABASE = "postgres"
-USERNAME="jobby"
-PASSWORD="12345678"
-PORT="5432"
+ENDPOINT ="****" 
+DATABASE = "****"
+USERNAME="****"
+PASSWORD="****"
+PORT="****"
 
 DATABASE_URL = f"host='{ENDPOINT}' dbname='{DATABASE}' user='{USERNAME}' password='{PASSWORD}' port='{PORT}'"
