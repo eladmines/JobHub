58493265b82c760487ea53afda372adf22aea5cb
Elad Mines <mineselad@gmail.com>
2024-04-22 17:48:54 +0300

    Convert data tuple to list for remove saved job action

--- app/savedjobs/actions.py
+++ app/savedjobs/actions.py
@@ -1,12 +1,13 @@
 from dbConnections import openConnection, closeConnection     
 from savedjobs.queries import GET_SAVED_JOBS,REMOVE_SAVED_JOB
-
+import json
 def getSavedJobs(data):
      con=openConnection()
      curs=con.cursor()
      try:
           curs.execute(GET_SAVED_JOBS,(data['sentData'],))
           data = curs.fetchone()[0]
+          print(data)
           return data
      except Exception as e:
           print(f"Error: {e}")
@@ -17,8 +18,12 @@ def removeSavedJob(data):
      con=openConnection()
      curs=con.cursor()
      try:
-          curs.execute(REMOVE_SAVED_JOB)
+          curs.execute(GET_SAVED_JOBS,(data['sentData'],))
           data = curs.fetchone()[0]
+          data = list(data)
+          for item in data:
+               res = json.loads(item)
+              
           return data
      except Exception as e:
           print(f"Error: {e}")
