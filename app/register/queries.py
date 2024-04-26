967e630e150834faffe65a119ca8322b4311735d
Elad Mines <mineselad@gmail.com>
2024-04-26 23:39:00 +0300

    Queries.py includes only postgresql queries (generate_insert_query in actions.py)

--- app/register/queries.py
+++ app/register/queries.py
@@ -1,31 +1,6 @@
-
-from dbConnections import openConnection, closeConnection
 INSERT_USER = "INSERT INTO users (firstName,lastName,email,password, role, company) " \
-                    "VALUES (%s, %s, %s, %s, %s, %s)"
-                    
+                    "VALUES (%s, %s, %s, %s, %s, %s)"              
 CHECK_EMAIL_EXISTS = "SELECT EXISTS (SELECT 1 FROM users WHERE email=%s)"
 
-
-def generate_insert_query(data):
-     con=openConnection()
-     curs=con.cursor()
-     try:
-          values=(data['firstName'], data['lastName'], data['email'],
-          data['inputPassword'], data['role'], data['company'])
-          print(data)
-          curs.execute(CHECK_EMAIL_EXISTS,(values[2],))
-          
-          exists = curs.fetchone()[0]
-          if (exists == False):
-               curs.execute(INSERT_USER,values)
-               con.commit()
-               return True
-          else:
-               return False   
-     except Exception as e:
-          print(f"Error: {e}")
-          # Rollback changes in case of an error
-          con.rollback() 
-   
 
     
