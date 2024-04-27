3cc4465903216352d2a6d99fed88a937924bc6a9
Elad Mines <mineselad@gmail.com>
2024-04-27 23:44:43 +0300

    Add user's details constants

--- app/register/actions.py
+++ app/register/actions.py
@@ -1,17 +1,22 @@
 
 from dbConnections import openConnection, closeConnection
-from register.actions import CHECK_EMAIL_EXISTS,INSERT_USER
-
+from register.queries import CHECK_EMAIL_EXISTS,INSERT_USER
+FIRST_NAME='firstName'
+LAST_NAME='lastName'
+EMAIL='email'
+PASSWORD='inputPassword'
+ROLE='role'
+COMPANY='company'
 
 def generate_insert_query(data):
+     #
      con=openConnection()
      curs=con.cursor()
      try:
-          values=(data['firstName'], data['lastName'], data['email'],
-          data['inputPassword'], data['role'], data['company'])
+          values=(data[FIRST_NAME], data[LAST_NAME], data[EMAIL],
+          data[PASSWORD], data[ROLE], data[COMPANY])
           print(data)
           curs.execute(CHECK_EMAIL_EXISTS,(values[2],))
-          
           exists = curs.fetchone()[0]
           if (exists == False):
                curs.execute(INSERT_USER,values)
