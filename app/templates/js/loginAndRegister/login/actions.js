929b8c4b0e17edb2137cb0bd21c8a740aca3fb48
Elad Mines <mineselad@gmail.com>
2024-05-02 23:34:08 +0300

    Add consts for html attributes

--- app/templates/js/loginAndRegister/login/actions.js
+++ app/templates/js/loginAndRegister/login/actions.js
@@ -1,6 +1,8 @@
+const INPUT_EMAIL="InputEmail"
+const INPUT_PASSWORD="InputPassword"
 function initArgs(){
-    var username = document.getElementById("InputEmail")
-    var password = document.getElementById("InputPassword")
+    var username = document.getElementById(INPUT_EMAIL)
+    var password = document.getElementById(INPUT_PASSWORD)
     var detailsArr=[username,password]
     inputsValidiation(detailsArr)
 }
