cdfaaf167ab3243962a1e1acf878e7e9213f5299
Elad Mines <mineselad@gmail.com>
2024-05-04 23:35:41 +0300

    Add an 'id' argument to the 'saveJob' function to handle cases where the job already exists.

--- app/jobs/actions.py
+++ app/jobs/actions.py
@@ -3,6 +3,7 @@ from jobs.queries import SAVE_JOB
 import json
 from models.job import Job
 from .queries import GET_ALL_JOBS
+
 def getAllJobs(con,curs):
     curs.execute(GET_ALL_JOBS)
     con.commit()
@@ -10,20 +11,20 @@ def getAllJobs(con,curs):
     Jobs=[]
     row = curs.fetchone()
     while row is not None:
-        job = Job(row[0],row[1],row[2],row[3], row[4],row[5],row[6],row[7])
+        job = Job(row[0],row[1],row[2],row[3], row[4],row[5],row[6],row[7],row[8])
         Jobs.append(job)
         rowCounter = rowCounter + 1
         row = curs.fetchone()
     return Jobs
 
-def saveJob(email,data):
+def saveJob(email,data,id):
      con=openConnection()
      curs=con.cursor()
      try:
           stringData = json.dumps(data)
-          print(type(stringData))
           curs.execute(SAVE_JOB,(stringData,email))
           con.commit() 
+          con=closeConnection()
      except Exception as e:
           print(f"Error: {e}")
           # Rollback changes in case of an error
