0757046952f7d1aa1723a4efc5821268fdd1c284
Elad Mines <mineselad@gmail.com>
2024-05-08 23:45:05 +0300

    REMOVE_SAVED_JOB query fixed

--- app/savedjobs/queries.py
+++ app/savedjobs/queries.py
@@ -1,7 +1,14 @@
 GET_SAVED_JOBS="SELECT savedjobs FROM users WHERE email=%s"
 
-REMOVE_SAVED_JOB="""UPDATE users 
-SET savedjobs = %s::jsonb
-WHERE email = %s;"""
-
+REMOVE_SAVED_JOB="""UPDATE users
+SET savedjobs = (
+    SELECT jsonb_agg(job)
+    FROM jsonb_array_elements(savedjobs) AS job
+    WHERE job->>'id' != %s
+)
+WHERE EXISTS (
+    SELECT 1
+    FROM jsonb_array_elements(savedjobs) AS job
+    WHERE job->>'id' = %s
+);"""
 
