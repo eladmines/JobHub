07a18f6cf4eb160db5aa3f82f5ee8e1a3f29d237
Elad Mines <mineselad@gmail.com>
2024-05-09 23:06:20 +0300

    Initializing jsonb[] column (savedjobs) in INSERT_USER query

--- app/register/queries.py
+++ app/register/queries.py
@@ -1,5 +1,5 @@
-INSERT_USER = "INSERT INTO users (firstName,lastName,email,password, role, company) " \
-                    "VALUES (%s, %s, %s, %s, %s, %s)"              
+INSERT_USER = "INSERT INTO users (firstName,lastName,email,password, role, company,savedjobs) " \
+                    "VALUES (%s, %s, %s, %s, %s, %s,'[]')"              
 CHECK_EMAIL_EXISTS = "SELECT EXISTS (SELECT 1 FROM users WHERE email=%s)"
 
 
