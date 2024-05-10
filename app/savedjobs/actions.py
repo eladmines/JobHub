12e71227bd951ba37d15db4c7221f8fcb629da18
Elad Mines <mineselad@gmail.com>
2024-05-10 23:38:20 +0300

    Send only the job ID instead of the entire job details in the removeSavedJob function.

--- app/savedjobs/actions.py
+++ app/savedjobs/actions.py
@@ -17,19 +17,10 @@ def removeSavedJob(data):
      con=openConnection()
      curs=con.cursor()
      try:
-          curs.execute(GET_SAVED_JOBS,(data['sentData'][0],))
-          savedJobs = curs.fetchone()[0]
-          link = data['sentData'][2]
-          savedJobs = list(savedJobs)
-          for item in savedJobs:
-               res = json.loads(item)
-               if res['link'] == link:
-                    savedJobs.remove(item)
-          savedJobs = json.dumps(savedJobs)
-          curs.execute(REMOVE_SAVED_JOB,(savedJobs,data['sentData'][0],))
+          email='data[\'sentData\'][0]'
+          jobId=str(data['sentData'][1])
+          curs.execute(REMOVE_SAVED_JOB,(jobId),email)
           con.commit() 
-          curs.execute(GET_SAVED_JOBS,(data['sentData'][0],))
-          savedJobs = curs.fetchone()[0]
           return data
      except Exception as e:
           print(f"Error: {e}")
