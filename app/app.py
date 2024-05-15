068fbf48b900f91c83ccf03fa9af5293fefad459
Elad Mines <mineselad@gmail.com>
2024-05-15 22:40:44 +0300

    Insert a register_blueprint for profile page

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ from companies.routes import companies_bp
 from login.routes import login_bp
 from register.routes import register_bp
 from savedjobs.routes import savedjobs_bp
+from profile.routes import profile_bp
 #from app.app import app
 app = Flask(__name__,static_folder='templates')
 app.register_blueprint(main_bp)    
@@ -13,6 +14,7 @@ app.register_blueprint(companies_bp)
 app.register_blueprint(login_bp)  
 app.register_blueprint(register_bp)  
 app.register_blueprint(savedjobs_bp)
+app.register_blueprint(profile_bp)
 if __name__ == '__main__':
     app.run(debug=False)
 
