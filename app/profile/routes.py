440632dc636396fdc49ac691bcb7734972f934a1
Elad Mines <mineselad@gmail.com>
2024-05-17 22:39:05 +0300

    Implement handle_post_request - Currently not working. Issue: Problem with getting data from JavaScript.To be fixed tomorrow.

--- app/profile/routes.py
+++ app/profile/routes.py
@@ -1,5 +1,11 @@
 from flask import Blueprint, render_template,request, jsonify
 profile_bp = Blueprint("profile_bp", __name__ , template_folder='profile')
-@profile_bp.route("/profile")
-def index():
+
+
+    
+
+@profile_bp.route("/profile", methods=["GET","POST"])
+def handle_post_request():
+    data = request.get_json()
+    print(data)
     return render_template('profile.html') 
