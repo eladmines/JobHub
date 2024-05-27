ba0431a24cf69bdec9f87ad10086d005af0ae33d
Elad Mines <mineselad@gmail.com>
2024-05-27 22:27:21 +0300

    Add user js class

---
+++ app/templates/js/models/user.js
@@ -0,0 +1,10 @@
+export class User{
+    constructor(id,firstname,lastname,email,role,company){
+        this.id=id
+        this.firstname=firstname
+        this.lastname=lastname
+        this.email=email
+        this.role = role
+        this.company=company
+    }
+}
