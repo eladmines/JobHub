26fa31a9b8ad0dc847b3fc9f4c967b8881707597
Elad <63946625+eladmines@users.noreply.github.com>
2024-06-13 09:03:59 +0300

    Add savedjobs,experience,skills attributes

--- app/templates/js/models/user.js
+++ app/templates/js/models/user.js
@@ -1,11 +1,14 @@
 export class User{
-    constructor(id,firstname,lastname,email,role,company){
-        this.id=id
-        this.firstname=firstname
-        this.lastname=lastname
-        this.email=email
-        this.role = role
-        this.company=company
+    constructor(id,firstname,lastname,email,role,company,experience,skills){
+        this.id=id;
+        this.firstname=firstname;
+        this.lastname=lastname;
+        this.email=email;
+        this.savedjobs=savedjobs;
+        this.role = role;
+        this.company=company;
+        this.experience=experience;
+        this.skills=skills;
     }
 }
 
