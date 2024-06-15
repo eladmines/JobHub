4fddaffef1c2b3a4b8106569c9cfe3bef7ca07e0
Elad <63946625+eladmines@users.noreply.github.com>
2024-06-15 23:43:58 +0300

    Update 'SAVE_COMMENT', 'GET_COMMENTS', 'GET_SAVED_JOBSIDS' , 'DELETE_SAVED_JOB' queries

--- app/savedjobs/queries.py
+++ app/savedjobs/queries.py
@@ -1,27 +1,10 @@
-GET_SAVED_JOBS="SELECT savedjobs FROM users WHERE email=%s"
+#GET_SAVED_JOBS="SELECT savedjobs FROM users WHERE email=%s"
+GET_SAVED_JOBSIDS="SELECT job_id FROM jobs_saved WHERE user_id=%s"
 
+GET_SAVED_JOBS="SELECT * FROM jobs WHERE id=%s"
 
-REMOVE_SAVED_JOB="""UPDATE users 
-SET savedjobs = (
-    CASE 
-        WHEN savedjobs IS NULL OR jsonb_array_length(savedjobs) = 0 THEN
-            jsonb_build_array()  -- Empty JSON array
-        ELSE
-            (SELECT jsonb_agg(job)
-             FROM jsonb_array_elements(savedjobs) AS job
-             WHERE job->>'id' != %s)
-    END
-)
-WHERE email = %s;"""
+DELETE_SAVED_JOB="DELETE FROM jobs_saved WHERE user_id=%s AND job_id=%s"
 
-REMOVE_SAVED_JOB="""UPDATE users 
-SET savedjobs = (
-    SELECT jsonb_agg(job)
-    FROM jsonb_array_elements(savedjobs) AS job
-    WHERE job->>'id' != %s
-) WHERE email = %s;
-
-"""
 
 INIT_EMPTY_SAVEDJOBS_ARR="""
 UPDATE users 
@@ -35,3 +18,11 @@ SET savedjobs = (
 )
 WHERE email = %s;
 """
+
+SAVE_COMMENT="""UPDATE jobs_saved
+SET comments = comments || jsonb_build_object(CURRENT_TIMESTAMP,%s)
+WHERE (job_id = %s AND user_id = %s);
+"""
+
+
+GET_COMMENTS="""SELECT comments from jobs_saved where user_id=%s AND job_id=%s"""
