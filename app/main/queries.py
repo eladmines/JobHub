4ec1279bff7817f5672768ba88722f56c01b59ce
Elad <63946625+eladmines@users.noreply.github.com>
2024-06-16 23:01:45 +0300

    Implement 'GET_NUM_OF_SAVED_JOBS' query

--- app/main/queries.py
+++ app/main/queries.py
@@ -1,1 +1,3 @@
-GET_USER_DETAILS="SELECT * FROM users WHERE email=%s"
+GET_USER_DETAILS="""SELECT * FROM users WHERE email=%s"""
+
+GET_NUM_OF_SAVED_JOBS="""SELECT COUNT(*) AS row_count FROM jobs_saved where user_id=%s"""
