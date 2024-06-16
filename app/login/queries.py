f8a8fee33f77c65f276439cdaf2209e9e835ccd0
Elad <63946625+eladmines@users.noreply.github.com>
2024-06-16 23:37:26 +0300

    Delete unnecessary comment.

--- app/login/queries.py
+++ app/login/queries.py
@@ -1,1 +1,2 @@
-CHECK_LOGIN_DETAILS= "SELECT EXISTS (SELECT 1 FROM users WHERE email=%s and password=%s)"
+#CHECK_LOGIN_DETAILS= "SELECT EXISTS (SELECT 1 FROM users WHERE email=%s and password=%s)"
+CHECK_LOGIN_DETAILS="SELECT id FROM users WHERE email = %s AND password = %s"
