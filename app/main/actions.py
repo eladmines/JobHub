cb2c2ab1b80e2ae9c676d1e6bb279d3206fe5cb3
Elad <63946625+eladmines@users.noreply.github.com>
2024-06-16 23:03:22 +0300

    Update getUserData to execute the GET_NUM_OF_SAVED_JOBS query.

--- app/main/actions.py
+++ app/main/actions.py
@@ -1,5 +1,5 @@
 from app.dbConnections import openConnection, closeConnection     
-from app.main.queries import GET_USER_DETAILS
+from app.main.queries import GET_USER_DETAILS,GET_NUM_OF_SAVED_JOBS
 
 def getUserData(data):
      con=openConnection()
@@ -7,6 +7,9 @@ def getUserData(data):
      try:
           res = curs.execute(GET_USER_DETAILS,(data,))
           res = curs.fetchone()
+          savedJobsCounter = curs.execute(GET_NUM_OF_SAVED_JOBS,(res[0],))
+          savedJobsCounter = curs.fetchone()
+          res = res + savedJobsCounter
           closeConnection(con)
           return res
      except Exception as e:
