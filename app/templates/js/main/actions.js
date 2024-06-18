5bef445c19efe2b7e3ea85ffbeedfcf0eecb451c
Elad <63946625+eladmines@users.noreply.github.com>
2024-06-18 19:11:37 +0300

    Set saved_jobs_counter

--- app/templates/js/main/actions.js
+++ app/templates/js/main/actions.js
@@ -13,10 +13,12 @@ function checkCookies(){
 
 async function initPage(){
     var userData = await getUserData();
-    document.getElementById("role").innerText=userData.role
-    document.getElementById("company").innerText=userData.company
-    document.getElementById("experience").innerText=userData.experience
-    document.getElementById("skills").innerText=userData.skills
+    console.log(userData)
+    document.getElementById("role").innerText=userData.role;
+    document.getElementById("company").innerText=userData.company;
+    document.getElementById("experience").innerText=userData.experience;
+    document.getElementById("skills").innerText=userData.skills;
+    document.getElementById("savedjobs-counter").innerText=userData.saved_jobs_counter;
 }
 
 /*Functions to be executed */ 
