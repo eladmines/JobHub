57d195e0b8b40223bc2531ad0c698c2df551e074
Elad <63946625+eladmines@users.noreply.github.com>
2024-06-18 18:32:35 +0300

    Assign a name to the data[i] variable for readability and clarification.

--- app/savedjobs/routes.py
+++ app/savedjobs/routes.py
@@ -1,8 +1,8 @@
 
 from flask import Blueprint, render_template,request
-from app.savedjobs.actions import getSavedJobs,removeSavedJob
-
-
+from app.savedjobs.actions import getSavedJobs,removeSavedJob,saveComment,getComments
+import json
+from app.utils import removeSpecialChars
 savedjobs_bp = Blueprint("savedjobs_bp", __name__ , template_folder='savedjobs')
 @savedjobs_bp.route("/savedjobs")
 def index():
@@ -11,11 +11,28 @@ def index():
 @savedjobs_bp.route("/savedjobs", methods=["POST"])
 def handle_post_request():
     data = request.get_json()
-    if data['action'] == "get saved jobs":
+    action = data['action']
+    userId=data['sentData'][0]
+    if action == "get saved jobs":
         savedJobs=getSavedJobs(data)
+        jobsList =[]
+        for job in savedJobs:
+            job = vars(job)
+            removeSpecialChars(job)
+            jobsList.append(job)
+        savedJobs = json.dumps(jobsList)
         return savedJobs
-    if data['action'] == "remove saved job":
+    elif action == "remove saved job":
         res = removeSavedJob(data)
+        return res
+    elif action == "insert a comment":
+        jobId=data['sentData'][1]
+        dataForaAction=data['sentData'][2]
+        res=saveComment(int(userId),dataForaAction,jobId)
+        return res
+    elif action == "get comments":
+        jobId=data['sentData'][1]
+        res=getComments(int(userId),jobId)
         return res
 
     
