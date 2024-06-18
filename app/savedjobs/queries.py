5862e8fd0142ca207d0da842e8fa933a37c9610d
Elad <63946625+eladmines@users.noreply.github.com>
2024-06-18 18:07:16 +0300

    Delete an old GET_SAVED_JOBS query as it is not necessary.

--- app/savedjobs/queries.py
+++ app/savedjobs/queries.py
@@ -1,10 +1,8 @@
-#GET_SAVED_JOBS="SELECT savedjobs FROM users WHERE email=%s"
 GET_SAVED_JOBSIDS="SELECT job_id FROM jobs_saved WHERE user_id=%s"
 
 GET_SAVED_JOBS="SELECT * FROM jobs WHERE id=%s"
 
 DELETE_SAVED_JOB="DELETE FROM jobs_saved WHERE user_id=%s AND job_id=%s"
-
 
 INIT_EMPTY_SAVEDJOBS_ARR="""
 UPDATE users 
@@ -20,9 +18,9 @@ WHERE email = %s;
 """
 
 SAVE_COMMENT="""UPDATE jobs_saved
-SET comments = comments || jsonb_build_object(CURRENT_TIMESTAMP,%s)
+SET comments = comments || jsonb_build_object(CURRENT_DATE,%s)
 WHERE (job_id = %s AND user_id = %s);
 """
 
-
 GET_COMMENTS="""SELECT comments from jobs_saved where user_id=%s AND job_id=%s"""
+
