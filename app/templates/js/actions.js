e1e1220b0881d25fcf0a482f62b7e54ef2e97e67
Elad <63946625+eladmines@users.noreply.github.com>
2024-06-18 19:08:29 +0300

    Implement the getCookie function to filter specific cookies.

--- app/templates/js/actions.js
+++ app/templates/js/actions.js
@@ -1,6 +1,6 @@
 import {User} from './models/user.js'
 /* Send data to a page with the required action */ 
-export function sendData(page, sentData, action) {
+export  function sendData(page, sentData, action) {
     let data = { action: action, sentData: sentData }; // Wrap data in an object
     return fetch(page, {
         method: 'POST',
@@ -25,18 +25,18 @@ export function sendData(page, sentData, action) {
 }
 
 export async function getUserData() {
-    let cookies = document.cookie;
+    let emailCookie = getCookie("email");
     try {
-        const response = await fetch('/main', {
+            const response = await fetch('/main', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(cookies),
+            body: JSON.stringify(emailCookie),
         });
         if (response.ok) {
             const userData = await response.json();
-            var user=new User(userData[0],userData[1],userData[2],userData[3],userData[7],userData[5],userData[6],userData[8],userData[9]);
+            var user=new User(userData[0],userData[1],userData[2],userData[3],userData[7],userData[5],userData[6],userData[8],userData[9],userData[10]);
             return user;
         } else {
             throw new Error('Something went wrong');
@@ -45,3 +45,20 @@ export async function getUserData() {
         console.error(error);
     }
 }
+
+export function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
