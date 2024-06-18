f9c1cf92739e632d58f10d23b90c208da17b30a0
Elad <63946625+eladmines@users.noreply.github.com>
2024-06-18 18:00:58 +0300

    Initialize a new route to handle the POST request - doesn't work yet.

--- app/profile/routes.py
+++ app/profile/routes.py
@@ -2,5 +2,10 @@ from flask import Blueprint, render_template
 profile_bp = Blueprint("profile_bp", __name__ , template_folder='profile')
 
 @profile_bp.route("/profile", methods=["GET"])
+def handle_get_request():
+    return render_template('profile.html') 
+
+@profile_bp.route("/profile", methods=["POST"])
 def handle_post_request():
+    ### Here to call UPDATE_USER_DETAILS query
     return render_template('profile.html') 
