7f5cd42ff946d1ec51c12ae0474d21451a81186b
Elad <63946625+eladmines@users.noreply.github.com>
2024-06-23 21:47:10 +0300

    Add functions descriptions as comments.

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,21 @@
+from app.dbConnections import openConnection, closeConnection     
+
+#Remove special characters from JSON
 def removeSpecialChars(job):
     job["title"]=job["title"].replace("'"," ")
     job["description"] = job["description"].translate(str.maketrans({"'": " ", "\n": "<br>", '"': ""}))
     job["qualifications"] = job["qualifications"].translate(str.maketrans({"'": " ", "\n": "<br>", "\\": "", '{': " ", '}': " ", '"': " "}))
     return job
+
+#Connect->Execute->Close connection
+def queryExec(userId,jobId,query):
+     con=openConnection()
+     curs=con.cursor()
+     try:
+          curs.execute(query,(userId,jobId))
+          con.commit() 
+          closeConnection(con)
+     except Exception as e:
+          print(f"Error: {e}")
+          # Rollback changes in case of an error
+          con.rollback()
