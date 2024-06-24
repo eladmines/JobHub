98f24332fd4eacce2636daa2b15727ae8cdf5311
Elad <63946625+eladmines@users.noreply.github.com>
2024-06-24 23:16:06 +0300

    Implement a python Profile class, and add experience and skills attributes in user class.

---
+++ app/models/profile.py
@@ -0,0 +1,6 @@
+class Profile:
+    def __init__(self,id,numOfSavedJobs,numOfTodayApplications):
+        self.id=id
+        self.numOfSavedJobs=numOfSavedJobs
+        self.numOfTodayApplications=numOfTodayApplications
+
--- app/models/user.py
+++ app/models/user.py
@@ -1,8 +1,11 @@
 class User:
-    def __init__(self,email,password,firstname,lastname,role,company):
+    def __init__(self,email,password,firstname,lastname,role,company,id,experience,skills):
         self.email=email
         self.password=password
         self.firstname=firstname
         self.lastname=lastname
         self.role=role
         self.company=company
+        self.id=id
+        self.experience=experience
+        self.skills=skills
