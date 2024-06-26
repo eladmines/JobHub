eb18becdc01ea8948a2edd33c4e458c5c9a299d3
Elad <63946625+eladmines@users.noreply.github.com>
2024-06-26 23:10:04 +0300

    Delete SAVE_JOB_APPLICATION , DELETE_APPLICATION, GET_NUM_OF_APPLICATIONS_TODAY,GET__NUM_APPLICATIONS_THIS_WEEK,GET__NUM_APPLICATIONS_THIS_MONTH queries(Relocate to applications\queries.py)

--- app/main/queries.py
+++ app/main/queries.py
@@ -1,5 +1,1 @@
 GET_USER_DETAILS="""SELECT * FROM users WHERE email=%s"""
-
-GET_NUM_OF_SAVED_JOBS="""SELECT COUNT(*) AS row_count FROM jobs_saved where user_id=%s"""
-
-GET_NUM_OF_APPLICATIONS_TODAY="""SELECT COUNT(*) AS row_count FROM jobs_applicated where user_id=%s and date=CURRENT_DATE"""
