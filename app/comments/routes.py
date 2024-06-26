03ed8b585443af7530f42093c3abd753c46d75b4
Elad <63946625+eladmines@users.noreply.github.com>
2024-06-26 21:30:00 +0300

    Update handle_post_request function for handle request from the savedjobs and applications endpoints

--- app/comments/routes.py
+++ app/comments/routes.py
@@ -6,14 +6,28 @@ comments_bp = Blueprint("comments_bp", __name__ , template_folder='comments')
 @comments_bp.route("/comments", methods=["POST"])
 def handle_post_request():
     data = request.get_json()
-    action = data['action']
+    action = data['action'].split(" ")[0]
     userId=data['sentData'][0]
-    if action == "get comments":
+    if action in "get comments":
+        endpoint=data['action'].split(" ")[2]
         jobId=data['sentData'][1]
-        res=getComments(int(userId),jobId)
+        if "application" in endpoint:
+            endpoint="jobs_applicated"
+            res=getComments(endpoint,int(userId),jobId)
+        if "saved" in endpoint:
+            endpoint="jobs_saved"
+            res=getComments(endpoint,int(userId),jobId)
         return res
-    elif action == "insert a comment":
+    
+    elif action in "insert a comment":
+        endpoint=data['action'].split(" ")[3]
         jobId=data['sentData'][1]
         dataForaAction=data['sentData'][2]
-        res=saveComment(int(userId),dataForaAction,jobId)
+        if "application" in endpoint:
+            endpoint="jobs_applicated"
+            res=saveComment(endpoint,int(userId),dataForaAction,jobId)
+        if "saved" in endpoint:
+            endpoint="jobs_saved"
+            res=saveComment(endpoint,int(userId),dataForaAction,jobId)
         return res
+    return data
