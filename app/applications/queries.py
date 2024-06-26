d97dab79465fbbc18e219103212513ed63bc7e4e
Elad <63946625+eladmines@users.noreply.github.com>
2024-06-26 23:08:29 +0300

    Relocate SAVE_JOB_APPLICATION , DELETE_APPLICATION, GET_NUM_OF_APPLICATIONS_TODAY,GET__NUM_APPLICATIONS_THIS_WEEK,GET__NUM_APPLICATIONS_THIS_MONTH queries

--- app/applications/queries.py
+++ app/applications/queries.py
@@ -9,3 +9,15 @@ ON CONFLICT (user_id,job_id) DO NOTHING
 """
 
 DELETE_APPLICATION="DELETE FROM jobs_applicated WHERE user_id=%s AND job_id=%s"
+
+GET_NUM_OF_APPLICATIONS_TODAY="""SELECT COUNT(*) AS row_count FROM jobs_applicated where user_id=%s and date=CURRENT_DATE"""
+
+GET__NUM_APPLICATIONS_THIS_WEEK="""SELECT COUNT(*)
+FROM jobs_applicated
+WHERE date >= date_trunc('week', CURRENT_DATE)
+  AND date < date_trunc('week', CURRENT_DATE) + INTERVAL '1 week'; """
+
+GET__NUM_APPLICATIONS_THIS_MONTH="""SELECT COUNT(*)
+FROM jobs_applicated
+WHERE date >= date_trunc('month', CURRENT_DATE)
+  AND date < date_trunc('month', CURRENT_DATE) + INTERVAL '1 month'; """
