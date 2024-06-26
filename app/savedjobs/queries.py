381ce4d996217e6b97fcdfa50654a11b87a66c44
Elad <63946625+eladmines@users.noreply.github.com>
2024-06-26 23:25:45 +0300

    Delete unneccessary 'INIT_EMPTY_SAVEDJOBS_ARR'  and relocate 'SAVE_COMMENT' and 'GET_COMMENTS' queries , implement 'SAVE_JOB' ,'GET_NUM_OF_SAVED_JOBS' queries

--- app/savedjobs/queries.py
+++ app/savedjobs/queries.py
@@ -4,23 +4,10 @@ GET_SAVED_JOBS="SELECT * FROM jobs WHERE id=%s"
 
 DELETE_SAVED_JOB="DELETE FROM jobs_saved WHERE user_id=%s AND job_id=%s"
 
-INIT_EMPTY_SAVEDJOBS_ARR="""
-UPDATE users 
-SET savedjobs = (
-    CASE 
-        WHEN savedjobs IS NULL OR jsonb_array_length(savedjobs) = 0 THEN
-            jsonb_build_array() 
-    ELSE
-        savedjobs
-    END
-)
-WHERE email = %s;
+GET_NUM_OF_SAVED_JOBS="""SELECT COUNT(*) AS row_count FROM jobs_saved where user_id=%s"""
+
+SAVE_JOB = """
+INSERT INTO jobs_saved (user_id,job_id)
+VALUES (%s,%s)
+ON CONFLICT (user_id,job_id) DO NOTHING
 """
-
-SAVE_COMMENT="""UPDATE jobs_saved
-SET comments = comments || jsonb_build_object(CURRENT_DATE,%s)
-WHERE (job_id = %s AND user_id = %s);
-"""
-
-GET_COMMENTS="""SELECT comments from jobs_saved where user_id=%s AND job_id=%s"""
-
