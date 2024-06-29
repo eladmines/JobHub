e77648841177a4370f7c2fa25201670c6beb3330
Elad <63946625+eladmines@users.noreply.github.com>
2024-06-29 17:06:43 +0300

    Implement getter and setter methods in the 'User' and 'Job' classes

--- app/models/job.py
+++ app/models/job.py
@@ -1,14 +1,66 @@
-
 class Job:
-    def __init__(self,title,location,description,qualifications,date,link, company,image,id):
-        self.title=title
-        self.location=location
-        self.description=description
+    def __init__(self, title, location, description, qualifications, date, link, company, image, id):
+        self.title = title
+        self.location = location
+        self.description = description
         self.qualifications = qualifications
-        self.company=company
-        self.image = image
         self.date = date
-        self.link=link
-        self.id=id
+        self.link = link
+        self.company = company
+        self.image = image
+        self.id = id
 
+    def get_title(self):
+        return self.title
+    
+    def set_title(self, title):
+        self.title = title
+
+    def get_location(self):
+        return self.location
+    
+    def set_location(self, location):
+        self.location = location
+
+    def get_description(self):
+        return self.description
+    
+    def set_description(self, description):
+        self.description = description
+
+    def get_qualifications(self):
+        return self.qualifications
+    
+    def set_qualifications(self, qualifications):
+        self.qualifications = qualifications
+
+    def get_date(self):
+        return self.date
+    
+    def set_date(self, date):
+        self.date = date
+
+    def get_link(self):
+        return self.link
+    
+    def set_link(self, link):
+        self.link = link
+
+    def get_company(self):
+        return self.company
+    
+    def set_company(self, company):
+        self.company = company
+
+    def get_image(self):
+        return self.image
+    
+    def set_image(self, image):
+        self.image = image
+
+    def get_id(self):
+        return self.id
+    
+    def set_id(self, id):
+        self.id = id
         
--- app/models/user.py
+++ app/models/user.py
@@ -1,11 +1,65 @@
 class User:
-    def __init__(self,email,password,firstname,lastname,role,company,id,experience,skills):
-        self.email=email
-        self.password=password
-        self.firstname=firstname
-        self.lastname=lastname
-        self.role=role
-        self.company=company
-        self.id=id
-        self.experience=experience
-        self.skills=skills
+    def __init__(self, email, password, firstname, lastname, role, company, id, experience, skills):
+        self.email = email
+        self.password = password
+        self.firstname = firstname
+        self.lastname = lastname
+        self.role = role
+        self.company = company
+        self.id = id
+        self.experience = experience
+        self.skills = skills
+
+    def get_email(self):
+        return self.email
+    
+    def set_email(self, email):
+        self.email = email
+
+    def get_password(self):
+        return self.password
+    
+    def set_password(self, password):
+        self.password = password
+
+    def get_firstname(self):
+        return self.firstname
+    
+    def set_firstname(self, firstname):
+        self.firstname = firstname
+
+    def get_lastname(self):
+        return self.lastname
+    
+    def set_lastname(self, lastname):
+        self.lastname = lastname
+
+    def get_role(self):
+        return self.role
+    
+    def set_role(self, role):
+        self.role = role
+
+    def get_company(self):
+        return self.company
+    
+    def set_company(self, company):
+        self.company = company
+
+    def get_id(self):
+        return self.id
+    
+    def set_id(self, id):
+        self.id = id
+
+    def get_experience(self):
+        return self.experience
+    
+    def set_experience(self, experience):
+        self.experience = experience
+
+    def get_skills(self):
+        return self.skills
+    
+    def set_skills(self, skills):
+        self.skills = skills
