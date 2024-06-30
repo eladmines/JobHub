c70120452d5ca0263020144318a3de5e32644e7f
Elad <63946625+eladmines@users.noreply.github.com>
2024-06-30 23:55:06 +0300

    Update the 'USER_AUTHENTICATION' query to return all columns (*) instead of just 'id'.

--- app/login/queries.py
+++ app/login/queries.py
@@ -1,2 +1,2 @@
 #CHECK_LOGIN_DETAILS= "SELECT EXISTS (SELECT 1 FROM users WHERE email=%s and password=%s)"
-CHECK_LOGIN_DETAILS="SELECT id FROM users WHERE email = %s AND password = %s"
+USER_AUTHENTICATION="SELECT * FROM users WHERE email = %s AND password = %s"
