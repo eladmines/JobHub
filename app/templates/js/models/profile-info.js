8db04433ddcc2376d57c0bfaae85ee0e267f991b
Elad <63946625+eladmines@users.noreply.github.com>
2024-07-01 23:24:32 +0300

    Implement getters and setters for 'profile' and 'user' classes

--- app/templates/js/models/profile-info.js
+++ app/templates/js/models/profile-info.js
@@ -1,11 +1,68 @@
-export class Profile{
-    constructor(id,saved_jobs_counter,apply_today){
-        this.id=id;
-        this.saved_jobs_counter=saved_jobs_counter
-        this.apply_today=apply_today
+export class Profile {
+    constructor(id, savedJobsCount, dailyApplicationsCount, weeklyApplicationsCount, monthlyApplicationsCount, weeklyApplicationsGoal,arrayOfMonthlyApplicationsCount) {
+        this.id = id;
+        this.savedJobsCount = savedJobsCount;
+        this.dailyApplicationsCount = dailyApplicationsCount;
+        this.weeklyApplicationsCount = weeklyApplicationsCount;
+        this.monthlyApplicationsCount = monthlyApplicationsCount;
+        this.weeklyApplicationsGoal = weeklyApplicationsGoal;
+        this.arrayOfMonthlyApplicationsCount=arrayOfMonthlyApplicationsCount;
+    }
+
+    // Setters
+    setId(id) {
+        this.id = id;
+    }
+
+    setSavedJobsCount(savedJobsCount) {
+        this.savedJobsCount = savedJobsCount;
+    }
+
+    setDailyApplicationsCount(dailyApplicationsCount) {
+        this.dailyApplicationsCount = dailyApplicationsCount;
+    }
+
+    setWeeklyApplicationsCount(weeklyApplicationsCount) {
+        this.weeklyApplicationsCount = weeklyApplicationsCount;
+    }
+
+    setMonthlyApplicationsCount(monthlyApplicationsCount) {
+        this.monthlyApplicationsCount = monthlyApplicationsCount;
+    }
+
+    setWeeklyApplicationsGoal(weeklyApplicationsGoal) {
+        this.weeklyApplicationsGoal = weeklyApplicationsGoal;
+    }
+
+    setArrayOfMonthlyApplicationsCount(arrayOfMonthlyApplicationsCount) {
+        this.arrayOfMonthlyApplicationsCount = arrayOfMonthlyApplicationsCount;
+    }
+    // Getters
+    getId() {
+        return this.id;
+    }
+
+    getSavedJobsCount() {
+        return this.savedJobsCount;
+    }
+
+    getDailyApplicationsCount() {
+        return this.dailyApplicationsCount;
+    }
+
+    getWeeklyApplicationsCount() {
+        return this.weeklyApplicationsCount;
+    }
+
+    getMonthlyApplicationsCount() {
+        return this.monthlyApplicationsCount;
+    }
+
+    getWeeklyApplicationsGoal() {
+        return this.weeklyApplicationsGoal;
+    }
+
+    getArrayOfMonthlyApplicationsCount() {
+        return this.arrayOfMonthlyApplicationsCount;
     }
 }
-
-
-
-
--- app/templates/js/models/user.js
+++ app/templates/js/models/user.js
@@ -1,16 +1,86 @@
-export class User{
-    constructor(id,firstname,lastname,email,role,company,experience,skills){
-        this.id=id;
-        this.firstname=firstname;
-        this.lastname=lastname;
-        this.email=email;
+export class User {
+    constructor(id, firstname, lastname, email, password, role, company, experience, skills) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
         this.role = role;
-        this.company=company;
-        this.experience=experience;
-        this.skills=skills;
+        this.company = company;
+        this.experience = experience;
+        this.skills = skills;
+    }
+
+    // Setters
+    setFirstName(firstName) {
+        this.firstname = firstName;
+    }
+
+    setLastName(lastName) {
+        this.lastname = lastName;
+    }
+
+    setEmail(email) {
+        this.email = email;
+    }
+
+    setPassword(password) {
+        this.password = password;
+    }
+
+    setRole(role) {
+        this.role = role;
+    }
+
+    setCompany(company) {
+        this.company = company;
+    }
+
+    setExperience(experience) {
+        this.experience = experience;
+    }
+
+    setSkills(skills) {
+        this.skills = skills;
+    }
+
+    // Getters
+    getId() {
+        return this.id;
+    }
+
+    getFirstName() {
+        return this.firstname;
+    }
+
+    getLastName() {
+        return this.lastname;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getPassword() {
+        return this.password;
+    }
+
+    getRole() {
+        return this.role;
+    }
+
+    getCompany() {
+        return this.company;
+    }
+
+    getExperience() {
+        return this.experience;
+    }
+
+    getSkills() {
+        return this.skills;
     }
 }
-
 
 
 
