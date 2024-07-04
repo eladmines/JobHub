3a6b966e554d38c0e96cae05aa6abbc3ee6e036d
Elad <63946625+eladmines@users.noreply.github.com>
2024-07-04 17:29:25 +0300

    Implement the profile page function

--- app/templates/js/profile/actions.js
+++ app/templates/js/profile/actions.js
@@ -1,15 +1,30 @@
-import {getUserData} from '../actions.js'
-import {sendData} from '../actions.js'
-var user = await getUserData();
+import {sendData,getUserSession,removeSearchBar,getCookieValue} from '../utils.js'
+import {User} from '../models/user.js'
+
 async function initPage(){
+    var user = getUserSession();
     document.getElementById("firstName").value=user.firstname
     document.getElementById("lastName").value=user.lastname
     document.getElementById("Email").value=user.email
     document.getElementById("Role").value=user.role
     document.getElementById("Company").value=user.company
+    document.getElementById("Experience").value=user.experience
+    document.getElementById("Skills").value=user.skills
 }
-function changeUserDetails(){
+
+function changeUserDetails(user){
+    var firstName = document.getElementById("firstName").value
+    var lastName = document.getElementById("lastName").value
+    var email = document.getElementById("Email").value
+    var role = document.getElementById("Role").value
+    var company = document.getElementById("Company").value
+    var experience = document.getElementById("Experience").value
+    var skills = document.getElementById("Skills").value
+    var user = new User(getCookieValue('id'),firstName,lastName,email,"",role,company,experience,skills);
     sendData('/profile',user,'update user details')
 }
 /*Functions to be executed */ 
-initPage()
+initPage();
+removeSearchBar();
+var saveButton = document.getElementById("save-button");
+saveButton.addEventListener('click',changeUserDetails);
