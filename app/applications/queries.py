17380c0349b669c01936e5ca38fbe4ef1e0928f7
Elad <63946625+eladmines@users.noreply.github.com>
2024-07-04 15:52:50 +0300

    Implement GET__NUM_APPLICATIONS_THIS_MONTH query

--- app/applications/queries.py
+++ app/applications/queries.py
@@ -1,23 +1,52 @@
-GET_APPLICATIONS_JOBSIDS="SELECT job_id FROM jobs_applicated WHERE user_id=%s"
+GET_APPLICATIONS_JOBSIDS="""SELECT job_id,date FROM jobs_applicated WHERE user_id=%s"""
 
-GET_APPLICATED_JOBS="SELECT * FROM jobs WHERE id=%s"
+GET_APPLICATED_JOBS="""SELECT * FROM jobs WHERE id=%s"""
 
 SAVE_JOB_APPLICATION = """
-INSERT INTO jobs_applicated (user_id,job_id)
+INSERT INTO 
+    jobs_applicated (user_id,job_id)
 VALUES (%s,%s)
 ON CONFLICT (user_id,job_id) DO NOTHING
 """
 
 DELETE_APPLICATION="DELETE FROM jobs_applicated WHERE user_id=%s AND job_id=%s"
 
-GET_NUM_OF_APPLICATIONS_TODAY="""SELECT COUNT(*) AS row_count FROM jobs_applicated where user_id=%s and date=CURRENT_DATE"""
+GET_NUM_OF_APPLICATIONS_TODAY="""
+SELECT 
+    COUNT(*) AS row_count 
+FROM 
+    jobs_applicated where user_id=%s and date=CURRENT_DATE"""
 
-GET__NUM_APPLICATIONS_THIS_WEEK="""SELECT COUNT(*)
-FROM jobs_applicated
-WHERE date >= date_trunc('week', CURRENT_DATE)
-  AND date < date_trunc('week', CURRENT_DATE) + INTERVAL '1 week'; """
+GET__NUM_APPLICATIONS_THIS_WEEK="""
+SELECT 
+    COUNT(*)
+FROM 
+    jobs_applicated 
+WHERE 
+    user_id=%s AND date >= date_trunc('week', CURRENT_DATE) AND date < date_trunc('week', CURRENT_DATE) + INTERVAL '1 week'; """
+  
 
-GET__NUM_APPLICATIONS_THIS_MONTH="""SELECT COUNT(*)
-FROM jobs_applicated
-WHERE date >= date_trunc('month', CURRENT_DATE)
-  AND date < date_trunc('month', CURRENT_DATE) + INTERVAL '1 month'; """
+GET__NUM_APPLICATIONS_THIS_MONTH="""
+SELECT 
+    COUNT(*)
+FROM 
+    jobs_applicated
+WHERE 
+    user_id=%s AND date >= date_trunc('month', CURRENT_DATE)  AND date < date_trunc('month', CURRENT_DATE) + INTERVAL '1 month'; """
+ 
+
+
+GET_NUM_OF_APPLICATIONS_BY_MONTH="""
+SELECT 
+    EXTRACT(YEAR FROM date) AS year,
+    EXTRACT(MONTH FROM date) AS month,
+    COUNT(*) AS rows_inserted
+FROM 
+    jobs_applicated
+WHERE 
+    user_id = %s
+GROUP BY 
+    EXTRACT(YEAR FROM date), EXTRACT(MONTH FROM date)
+ORDER BY 
+    year, month;
+"""
