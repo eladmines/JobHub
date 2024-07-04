7374da1ad24f9166c8f2444554e430535ab3f14a
Elad <63946625+eladmines@users.noreply.github.com>
2024-07-04 16:32:01 +0300

    Implement set_array_of_monthly_applications_count function in the Profile class'

--- app/models/profile.py
+++ app/models/profile.py
@@ -1,11 +1,12 @@
 class Profile:
-    def __init__(self, id, saved_jobs_count, daily_applications_count, weekly_applications_count, monthly_applications_count, weekly_applications_goal):
+    def __init__(self, id, saved_jobs_count, daily_applications_count, weekly_applications_count, monthly_applications_count, weekly_applications_goal,array_of_monthly_applications_count):
         self.id = id
         self.saved_jobs_count = saved_jobs_count
         self.daily_applications_count = daily_applications_count
         self.weekly_applications_count = weekly_applications_count
         self.weekly_applications_goal = weekly_applications_goal
         self.monthly_applications_count = monthly_applications_count
+        self.array_of_monthly_applications_count=array_of_monthly_applications_count
 
     def get_id(self):
         return self.id
@@ -25,6 +26,9 @@ class Profile:
     def get_weekly_applications_goal(self):
         return self.weekly_applications_goal
     
+    def get_array_of_monthly_applications_count(self):
+        return self.array_of_monthly_applications_count
+    
     def set_id(self,new_value):
         self.id=new_value
 
@@ -43,11 +47,15 @@ class Profile:
     def set_weekly_applications_goal(self,new_value):
         self.weekly_applications_goal=new_value
 
+    def set_array_of_monthly_applications_count(self,new_value):
+        self.array_of_monthly_applications_count=new_value
+
     def __str__(self):
             return (f"Profile(id={self.id}, "
                     f"saved_jobs_count={self.saved_jobs_count}, "
                     f"daily_applications_count={self.daily_applications_count}, "
                     f"weekly_applications_count={self.weekly_applications_count}, "
                     f"monthly_applications_count={self.monthly_applications_count}, "
-                    f"weekly_applications_goal={self.weekly_applications_goal})")
+                    f"weekly_applications_goal={self.weekly_applications_goal},"
+                    f"weekly_applications_goal={self.array_of_monthly_applications_count})")
     
