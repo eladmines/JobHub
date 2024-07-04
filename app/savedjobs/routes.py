ca3c1afe485415af1b9ba5b0353017ea8a700c6e
Elad <63946625+eladmines@users.noreply.github.com>
2024-07-04 16:40:55 +0300

    Fix: Has the job been applied for?

--- app/savedjobs/routes.py
+++ app/savedjobs/routes.py
@@ -1,9 +1,11 @@
 
-from flask import Blueprint, render_template,request
-from app.savedjobs.actions import getSavedJobs,removeSavedJob,saveComment,getComments
+from flask import render_template,request
+from app.savedjobs.actions import get_saved_jobs,remove_saved_job,save_job
 import json
-from app.utils import removeSpecialChars
-savedjobs_bp = Blueprint("savedjobs_bp", __name__ , template_folder='savedjobs')
+from app.utils import remove_special_chars
+from app.applications.actions import get_applications_ids
+from app.savedjobs import savedjobs_bp
+
 @savedjobs_bp.route("/savedjobs")
 def index():
     return render_template('savedjobs.html') 
@@ -12,27 +14,28 @@ def index():
 def handle_post_request():
     data = request.get_json()
     action = data['action']
-    userId=data['sentData'][0]
+    userId=data['sentData']
     if action == "get saved jobs":
-        savedJobs=getSavedJobs(data)
+        savedJobs=get_saved_jobs(data)
+        applications = get_applications_ids(userId)
         jobsList =[]
         for job in savedJobs:
             job = vars(job)
-            removeSpecialChars(job)
+            job["saved"]="yes"
+            job["applied"]="no"
+            remove_special_chars(job)
+            for application in applications:
+                if job["id"] == application[0]:
+                    job["applied"]=application[1]   
             jobsList.append(job)
         savedJobs = json.dumps(jobsList)
         return savedJobs
     elif action == "remove saved job":
-        res = removeSavedJob(data)
+        res = remove_saved_job(data)
         return res
-    elif action == "insert a comment":
+    elif action == "save job":
         jobId=data['sentData'][1]
-        dataForaAction=data['sentData'][2]
-        res=saveComment(int(userId),dataForaAction,jobId)
-        return res
-    elif action == "get comments":
-        jobId=data['sentData'][1]
-        res=getComments(int(userId),jobId)
-        return res
-
+        save_job(userId[0],jobId)
+        return data
+    return data
     
