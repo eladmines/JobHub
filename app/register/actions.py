8f9fd931620ad84d830a66abb78e666042b0c29a
Elad <63946625+eladmines@users.noreply.github.com>
2024-07-04 16:37:11 +0300

    Revise and improve the registration process.

--- app/register/actions.py
+++ app/register/actions.py
@@ -1,27 +1,27 @@
 
-from app.dbConnections import openConnection, closeConnection
-from app.register.queries import CHECK_EMAIL_EXISTS,INSERT_USER
-FIRST_NAME='firstName'; LAST_NAME='lastName';EMAIL='email';PASSWORD='inputPassword';ROLE='role';COMPANY='company'
+from app.dbConnections import open_connection, close_connection
+from app.register.queries import REGISTER_USER
+from app.models.user import User
+FIRST_NAME='firstName'; LAST_NAME='lastName';EMAIL='email';PASSWORD='inputPassword';ROLE='role';COMPANY='company';EXPERIENCE='experience';SKILLS='skills'
 
-def generate_insert_query(data):
-     #
-     con=openConnection()
+def register_user(user):
+     con=open_connection()
      curs=con.cursor()
      try:
-          values=(data[FIRST_NAME], data[LAST_NAME], data[EMAIL],
-          data[PASSWORD], data[ROLE], data[COMPANY])
-          curs.execute(CHECK_EMAIL_EXISTS,(values[2],))
-          exists = curs.fetchone()[0]
-          if (exists == False):
-               curs.execute(INSERT_USER,values)
+          values=(user.get_firstname(), user.get_lastname(), user.get_email(),
+          user.get_password(), user.get_role(), user.get_company(),user.get_experience(),user.get_skills())
+          curs.execute(REGISTER_USER,values)
+          curs.fetchone()
+          if(curs.rowcount > 0):   
                con.commit()
+               close_connection(con)
                return True
-          else:
-               return False   
      except Exception as e:
           print(f"Error: {e}")
           # Rollback changes in case of an error
           con.rollback() 
-     con=closeConnection()
+          close_connection(con)
+          return False
+     
 
     
--- app/register/routes.py
+++ app/register/routes.py
@@ -1,7 +1,9 @@
 
 from flask import Blueprint, render_template,request, jsonify
-from app.register.actions import generate_insert_query
-register_bp = Blueprint("register_bp", __name__ , template_folder='register')
+from app.register.actions import register_user
+from app.models.user import User
+from app.register import register_bp
+
 @register_bp.route("/register")
 def index():
     return render_template('register.html') 
@@ -9,5 +11,17 @@ def index():
 @register_bp.route("/register", methods=["POST"])
 def handle_post_request():
     data = request.get_json()
-    uniqueEmail=generate_insert_query(data)
-    return jsonify(uniqueEmail)
+    id=data['sentData']['id']
+    firstname=data['sentData']['firstname']
+    lastname=data['sentData']['lastname']
+    email=data['sentData']['email']
+    password=data['sentData']['password']
+    role=data['sentData']['role']
+    company=data['sentData']['company']
+    experience=data['sentData']['experience']
+    skills=data['sentData']['skills']
+    user = User(id,firstname,lastname,email,password,role,company,experience,skills)
+    res = register_user(user)
+    return jsonify(res)
+
+
