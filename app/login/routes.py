7ef84da0f96bd3b3f38ed1a271e9d00c01f62e77
Elad <63946625+eladmines@users.noreply.github.com>
2024-07-04 16:25:34 +0300

    Revise user authentication process.

--- app/login/routes.py
+++ app/login/routes.py
@@ -1,8 +1,8 @@
 
-from flask import Blueprint, render_template,request, jsonify
-from app.login.actions import checkLoginDetails
-
-login_bp = Blueprint("login_bp", __name__ , template_folder='login')
+from flask import render_template,request, jsonify
+from app.login.actions import authentication
+from app.models.user import User
+from app.login import login_bp
 
 @login_bp.route("/")
 def index():
@@ -11,6 +11,11 @@ def index():
 @login_bp.route("/", methods=["POST"])
 def handle_post_request():
     data = request.get_json()
-    loginDetails=checkLoginDetails(data)
-    return jsonify(loginDetails)
-
+    email=data['sentData']['email']
+    password=data['sentData']['password']
+    loginDetails=authentication(email,password)
+    if loginDetails is not None:
+        user = User(loginDetails[0],loginDetails[1],loginDetails[2],loginDetails[3],loginDetails[4],loginDetails[5],loginDetails[6],loginDetails[7],loginDetails[8])
+        user=vars(user)
+        return user
+    return jsonify(None)
