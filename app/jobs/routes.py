ab47cd5c02be1eb1114d8b604cb6f68960e6f593
Elad <63946625+eladmines@users.noreply.github.com>
2024-07-04 16:23:38 +0300

    Rename function names to follow Python conventions.

--- app/jobs/routes.py
+++ app/jobs/routes.py
@@ -1,11 +1,12 @@
 from app.jobs import jobs_bp
 from flask import  render_template,request
-from app.dbConnections import openConnection, closeConnection
+from app.dbConnections import open_connection, close_connection
 import json
-from app.jobs.actions import getAllJobs
-from app.utils import removeSpecialChars
-from app.savedjobs.actions import GetSavedJobsIds
-from app.applications.actions import getApplicationsIds
+from app.jobs.actions import get_all_jobs
+from app.utils import remove_special_chars
+from app.savedjobs.actions import get_saved_jobs_ids
+from app.applications.actions import get_applications_ids
+
 @jobs_bp.route("/jobs")
 def index():
     return render_template('jobs.html') 
@@ -13,25 +14,29 @@ def index():
 @jobs_bp.route("/jobs", methods=["POST"])
 def handle_post_request():
     data = request.get_json()
-    con=openConnection()
+    con=open_connection()
     curs=con.cursor()
-    savedJobsIds=GetSavedJobsIds(data['sentData'])
-    applicationsJobsIds=getApplicationsIds(data['sentData'])
-    jobs=getAllJobs(curs)
+    savedJobsIds=get_saved_jobs_ids(data['sentData'])
+    applicationsJobsIds=get_applications_ids(data['sentData'])
+    jobs=get_all_jobs(curs)
     jobsList =[]
     for job in jobs:
         job = vars(job)
-        removeSpecialChars(job)
+        remove_special_chars(job)
+        job["saved"]="no"
+        job["applied"]="no"
         if(job['id'] in savedJobsIds):
             job["saved"]="yes"
         else:
             job["saved"]="no"
-        if(job['id'] in applicationsJobsIds):
-            job["applied"]="yes"
-        else:
-            job["applied"]="no"
+        for application in applicationsJobsIds:
+            if job['id']==application[0]:
+                job["applied"]=application[1]
+                break
+            else:
+                job["applied"]="no"
         jobsList.append(job)
     jobs = json.dumps(jobsList)
-    closeConnection(con)
+    close_connection(con)
     return jobs
 
--- app/savedjobs/actions.py
+++ app/savedjobs/actions.py
@@ -1,56 +1,56 @@
-from app.dbConnections import openConnection, closeConnection     
+from app.dbConnections import open_connection, close_connection     
 from app.savedjobs.queries import GET_SAVED_JOBSIDS,GET_SAVED_JOBS,DELETE_SAVED_JOB
 from app.models.job import Job
-from app.utils import queryExec
+from app.utils import query_exec
 from app.savedjobs.queries import SAVE_JOB
 
-def GetSavedJobsIds(userId):
-    con=openConnection()
+def get_saved_jobs_ids(userId):
+    con=open_connection()
     curs=con.cursor()
     curs.execute(GET_SAVED_JOBSIDS,(userId,))
     jobsIds=curs.fetchall()
     jobsIds = [x[0] for x in jobsIds]
     return jobsIds
 
-def getSavedJobs(data):
-     con=openConnection()
+def get_saved_jobs(data):
+     con=open_connection()
      curs=con.cursor()
      try:
           user_id=data['sentData']
-          jobsId=GetSavedJobsIds(user_id)
+          jobsId=get_saved_jobs_ids(user_id)
           Jobs=[]
           for item in jobsId:
                curs.execute(GET_SAVED_JOBS,(item,))
                job = curs.fetchone()
                job = Job(job[0],job[1],job[2],job[3], job[4],job[5],job[6],job[7],job[8])
                Jobs.append(job)
-          closeConnection(con)
+          close_connection(con)
           return Jobs
      except Exception as e:
           print(f"Error: {e}")
           # Rollback changes in case of an error
           con.rollback()
-          closeConnection(con) 
+          close_connection(con) 
 
 
-def removeSavedJob(data):
-     con=openConnection()
+def remove_saved_job(data):
+     con=open_connection()
      curs=con.cursor()
      try:
           user_id=(data['sentData'][0])
           jobId=str(data['sentData'][1])
           curs.execute(DELETE_SAVED_JOB,(user_id,jobId))
           con.commit()
-          closeConnection(con) 
+          close_connection(con) 
           return data
      except Exception as e:
           print(f"Error: {e}")
           # Rollback changes in case of an error
           con.rollback() 
-          closeConnection(con)
+          close_connection(con)
 
-def saveJob(userId,jobId):
-    queryExec(userId,jobId,SAVE_JOB)
+def save_job(userId,jobId):
+    query_exec(userId,jobId,SAVE_JOB)
 
 
 
