1763dc404d433e8c348f024ca59f2fc4d0a8a65b
Elad <63946625+eladmines@users.noreply.github.com>
2024-07-04 15:50:06 +0300

    Implement GET__NUM_APPLICATIONS_THIS_MONTH query

--- app/applications/routes.py
+++ app/applications/routes.py
@@ -1,42 +1,39 @@
-from flask import Blueprint, render_template
-from flask import  render_template,request
+from flask import render_template,request
+from app.applications.__init__ import applications_bp
+from app.applications.actions import get_applications,save_application,remove_application
+from app.utils import remove_special_chars
+from app.savedjobs.actions import get_saved_jobs_ids
 import json
-from app.applications.actions import getApplications,saveApplication,removeApplication
-from app.utils import removeSpecialChars
-from app.savedjobs.actions import GetSavedJobsIds
-applications_bp = Blueprint("applications_bp", __name__ , template_folder='applications')
+
 
 @applications_bp.route("/applications", methods=["GET"])
 def handle_get_request():
     return render_template('applications.html') 
 
-
 @applications_bp.route("/applications", methods=["POST"])
 def handle_post_request():
     data = request.get_json()
     action = data['action']
-    userId=data['sentData'][0]
-    print(action)
+    userId=data['sentData']
     if action == "get applications":
-        appliedJobs=getApplications(data)
+        appliedJobs=get_applications(data)
         jobsList =[]
         for job in appliedJobs:
-            job = vars(job)
-            job["applied"]="yes"
-            removeSpecialChars(job)
-            if job["id"] in GetSavedJobsIds(userId):
+            remove_special_chars(job)
+            if job["id"] in get_saved_jobs_ids(userId):
                 job["saved"]="yes"
             else:
                 job["saved"]="no"
             jobsList.append(job)
         appliedJobs = json.dumps(jobsList)
         return appliedJobs
+    
     elif action == "save application":
-        
         jobId=data['sentData'][1]
-        saveApplication(userId,jobId)
+        save_application(userId[0],jobId)
         return data
+    
     elif action == "remove job application":
-        res = removeApplication(data)
+        res = remove_application(data)
         return res
     return data
