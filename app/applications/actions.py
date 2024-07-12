cfcf275b8ed90d750324753e7f901b0f240eb348
Elad <63946625+eladmines@users.noreply.github.com>
2024-07-12 17:18:56 +0300

    Refactor save_application and remove_application for improved readability.

--- app/applications/actions.py
+++ app/applications/actions.py
@@ -1,51 +1,30 @@
 from app.dbConnections import open_connection, close_connection    
-from app.applications.queries import GET_APPLICATIONS_JOBSIDS,GET_APPLICATED_JOBS,SAVE_JOB_APPLICATION,DELETE_APPLICATION
+from app.applications.queries import GET_ALL_APPLICATIONS,SAVE_JOB_APPLICATION,DELETE_APPLICATION
 from app.models.job import Job
-from app.utils import query_exec
-
-def get_applications_ids(userId):
-    con=open_connection()
-    curs=con.cursor()
-    curs.execute(GET_APPLICATIONS_JOBSIDS,(userId,))
-    jobsIds=curs.fetchall()
-    jobsIds = [(item[0], f"{item[1].year}-{item[1].month:02}-{item[1].day:02}")  for item in jobsIds]
-    return jobsIds
+from app.utils import save_query_exec,remove_special_chars,delete_query_exec
+from flask import jsonify
 
 def get_applications(data):
-     con=open_connection()
-     curs=con.cursor()
-     try:
-          user_id=data['sentData']
-          applicationsId=get_applications_ids(user_id)
-          Jobs=[]
-          for item in applicationsId:
-               curs.execute(GET_APPLICATED_JOBS,(item[0],))
-               job = curs.fetchone()
-               job = Job(job[0],job[1],job[2],job[3], job[4],job[5],job[6],job[7],job[8])
-               job = vars(job)
-               job["applied"]=item[1]
-               Jobs.append(job)
-          close_connection(con)
-          return Jobs
-     except Exception as e:
-          print(f"Error: {e}")
-          close_connection(con) 
+    con = open_connection()
+    curs = con.cursor()
+    try:
+        Jobs = []
+        user_id = data['sentData']
+        curs.execute(GET_ALL_APPLICATIONS,(user_id))
+        jobs = curs.fetchall()
+        for job in jobs:
+            job = Job(job[0], job[1], job[2], job[3], job[4], job[5], job[6], job[7], job[8],job[9],job[10])
+            job = job.__dict__
+            job["description"]=remove_special_chars(job["description"])
+            Jobs.append(job)
+        close_connection(con)
+        return Jobs
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
-def save_application(userId,jobId):
-    query_exec(userId,jobId,SAVE_JOB_APPLICATION)
+def save_application(data):
+    save_query_exec(SAVE_JOB_APPLICATION,data)
+    
 
 def remove_application(data):
-     con=open_connection()
-     curs=con.cursor()
-     try:
-          user_id=(data['sentData'][0])
-          jobId=str(data['sentData'][1])
-          curs.execute(DELETE_APPLICATION,(user_id,jobId))
-          con.commit()
-          close_connection(con) 
-          return data
-     except Exception as e:
-          print(f"Error: {e}")
-          # Rollback changes in case of an error
-          con.rollback() 
-          close_connection(con)
+     delete_query_exec(DELETE_APPLICATION,data)
