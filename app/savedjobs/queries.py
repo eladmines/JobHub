ec50ea10ed0ad944b24b222fd59a960c4418a2ec
Elad <63946625+eladmines@users.noreply.github.com>
2024-07-15 00:35:09 +0300

    Update Jobs table columns

--- app/savedjobs/queries.py
+++ app/savedjobs/queries.py
@@ -11,3 +11,18 @@ INSERT INTO jobs_saved (user_id,job_id)
 VALUES (%s,%s)
 ON CONFLICT (user_id,job_id) DO NOTHING
 """
+
+GET_ALL_SAVED_JOBS="""SELECT 
+    jobs.*,
+    CASE WHEN jobs_saved.job_id IS NOT NULL THEN 1 ELSE 0 END AS saved,
+    jobs_applicated.date AS applicated_date
+FROM 
+    jobs
+LEFT JOIN 
+    jobs_saved ON jobs.job_id = jobs_saved.job_id AND jobs_saved.user_id = 4
+LEFT JOIN 
+    jobs_applicated ON jobs.job_id = jobs_applicated.job_id AND jobs_applicated.user_id = 4
+WHERE 
+    (jobs_saved.job_id IS NOT NULL AND jobs_saved.job_id = jobs.job_id)
+    ;
+"""
