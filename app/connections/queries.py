8abc924ae3ea9f6660a899b274884feb9a112592
Elad <63946625+eladmines@users.noreply.github.com>
2024-07-15 17:13:01 +0300

    Implement 'GET_CONNECTIONS' , 'ADD_CONNECTION' and 'DELETE_CONNECTION' queries

--- app/connections/queries.py
+++ app/connections/queries.py
@@ -1,1 +1,6 @@
 GET_CONNECTIONS="""SELECT * FROM connections where user_id=%s"""
+
+ADD_CONNECTION="""INSERT INTO connections(user_id,name,position,company,phone,accounts) VALUES (%s,%s,%s,%s,%s,%s)"""
+
+DELETE_CONNECTION= """DELETE FROM connections WHERE user_id=%s AND id=%s"""
+
