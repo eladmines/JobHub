05a3c0302df0c534892ab31b57e213d6cc43f5f6
Elad <63946625+eladmines@users.noreply.github.com>
2024-07-15 16:21:57 +0300

    Refactor executing functions to utilize psycopg2.extras for enhanced functionality.

--- app/utils.py
+++ app/utils.py
@@ -1,22 +1,52 @@
 from app.dbConnections import open_connection, close_connection     
+import psycopg2.extras
 
 #Remove special characters from JSON
-def remove_special_chars(job):
-    job["title"]=job["title"].replace("'"," ")
-    job["description"] = job["description"].translate(str.maketrans({"'": " ", "\n": "<br>", '"': ""}))
-    job["qualifications"] = job["qualifications"].translate(str.maketrans({"'": " ", "\n": "<br>", "\\": "", '{': " ", '}': " ", '"': " "}))
-    return job
+def remove_special_chars(item):
+     if item is None:
+          return None
+     item = item.translate(str.maketrans({"'": " ", "\n": " <br> ", "\\": "", '{': " ", '}': " ", '"': " "}))
+     return item
 
-#Connect->Execute->Close connection
-def query_exec(userId,jobId,query):
+#Connect->Execute (Save data)->Close connection
+def save_query_exec(query,data):
      con=open_connection()
      curs=con.cursor()
      try:
-          curs.execute(query,(userId,jobId))
+          curs.execute(query,data)
           con.commit() 
           close_connection(con)
+          return True
      except Exception as e:
           print(f"Error: {e}")
-          # Rollback changes in case of an error
           con.rollback()
+
+def delete_query_exec(query,data):
+     con=open_connection()
+     curs=con.cursor()
+     print(data)
+     try:
+          curs.execute(query,data)
+          con.commit() 
+          close_connection(con)
+          return True
+     except Exception as e:
+          print(f"Error: {e}")
+          con.rollback()
+
+#Get query execution - no commit neeeded
+def get_query_exec(query,data):
+     con=open_connection()
+     curs = con.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+     try:
+          curs.execute(query,(data))
+          data=curs.fetchall()
+          close_connection(con)
+          return data
+     except Exception as e:
+          print(f"Error: {e}")
+          con.rollback()
+
+
+
 
