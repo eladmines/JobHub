d1beee1f24d6e69aacae73a59497b83d6668fae6
Elad <63946625+eladmines@users.noreply.github.com>
2024-07-15 17:16:07 +0300

    Implement 'add_connection' and 'delete_connection' functions

--- app/connections/actions.py
+++ app/connections/actions.py
@@ -1,21 +1,28 @@
-from .queries import GET_CONNECTIONS
-from app.utils import get_query_exec
+from .queries import GET_CONNECTIONS,ADD_CONNECTION,DELETE_CONNECTION
+from app.utils import get_query_exec,save_query_exec,delete_query_exec
 from app.models.connection import Connection
 def get_connections(user_id):
-    rows = get_query_exec(GET_CONNECTIONS,user_id)
+    data=(user_id,)
+    rows = get_query_exec(GET_CONNECTIONS,data)
     if(rows is None):
             return "Error: get connections query failed",400
     connections=[]
     for row in rows:
-        id=row[0]
-        user_id=row[1]
-        name=row[2]
-        position=row[3]
-        company=row[4]
-        phone=row[5]
-        linkdin_account=row[6]
-        connection = Connection(id,user_id,name,position,company,phone,linkdin_account)
+        id=row['id']
+        user_id=row['user_id']
+        name=row['name']
+        position=row['position']
+        company=row['company']
+        phone=row['phone']
+        accounts=row['accounts']
+        connection = Connection(id,user_id,name,position,company,phone,accounts)
         connection= connection.__dict__
         connections.append(connection)  
     return connections,None
-    
+
+def add_connection(data):
+    save_query_exec(ADD_CONNECTION,data)
+
+
+def delete_connection(data):
+    delete_query_exec(DELETE_CONNECTION,data)
