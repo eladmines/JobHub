fc9e0729ced6096732bce0abb4ecab8d783727eb
Elad <63946625+eladmines@users.noreply.github.com>
2024-07-15 17:14:50 +0300

    Implement DELETE and POST method for delete and post connections

--- app/connections/routes.py
+++ app/connections/routes.py
@@ -1,22 +1,30 @@
 from flask import  render_template, jsonify,request
 from app.connections import connections_bp
-from app.connections.actions import get_connections
+from app.connections.actions import get_connections,add_connection,delete_connection
 @connections_bp.route("/connections")
 def index():
     return render_template('connections.html')
 
 @connections_bp.route("/connections/<user_id>" , methods=["GET"])
-
 def handle_get_request(user_id):
-    
     if(user_id is None):
         return jsonify("Error: user_id is null"),400
+    
     connections,error = get_connections(user_id)
     if error:
          return jsonify("Error: Failed to get connection"),400  
     return jsonify(connections),200
 
-
-@connections_bp.route("/connections/<user_id>", methods=["POST"])
-def handle_post_request(user_id):
+@connections_bp.route("/connections", methods=["POST"])
+def handle_post_request():
     data=request.get_json()
+    data = tuple(data['sentData'].values())[1:]
+    add_connection(data)
+    return jsonify(True)
+
+@connections_bp.route("/connections/<user_id>/<connection_to_delete>", methods=["DELETE"])
+def handle_delete_request(user_id,connection_to_delete):
+    data=[user_id,int(connection_to_delete)]
+    delete_connection(data)
+    return data
+
