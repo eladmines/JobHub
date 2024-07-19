ff60efc563622c68ee462d8272940d97c41670ea
Elad <63946625+eladmines@users.noreply.github.com>
2024-07-19 19:07:04 +0300

    Add ApplicationProcess class

---
+++ app/templates/js/models/ApplicationProcess.js
@@ -0,0 +1,67 @@
+export class ApplicationProcess {
+    constructor(id, jobId, date, interviewer, phone, status, notes) {
+        this._id = id; 
+        this._jobId = jobId;
+        this._date = date;
+        this._interviewer = interviewer;
+        this._phone = phone;
+        this._status = status;
+        this._notes = notes;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get jobId() {
+        return this._jobId;
+    }
+
+    get date() {
+        return this._date;
+    }
+
+    get interviewer() {
+        return this._interviewer;
+    }
+
+    get phone() {
+        return this._phone;
+    }
+
+    get status() {
+        return this._status;
+    }
+
+    get notes() {
+        return this._notes;
+    }
+    
+    set id(value) {
+        this._id = value;
+    }
+
+    set jobId(value) {
+        this._jobId = value;
+    }
+
+    set date(value) {
+        this._date = value;
+    }
+
+    set interviewer(value) {
+        this._interviewer = value;
+    }
+
+    set phone(value) {
+        this._phone = value;
+    }
+
+    set status(value) {
+        this._status = value;
+    }
+
+    set notes(value) {
+        this._notes = value;
+    }
+}
