16570cd6ab7fdb38d8c0407dfa12f2fca30e5db8
Elad <63946625+eladmines@users.noreply.github.com>
2024-08-01 23:56:29 +0300

    Update INSERT_JOBS query - return job_link in order to support sending emails

--- app/data/queries.py
+++ app/data/queries.py
@@ -1,3 +1,4 @@
 INSERT_JOBS = """INSERT INTO jobs (job_title, job_location, job_description, job_qualifications, job_date, job_link, job_company)
 VALUES (%s, %s, %s, %s, %s, %s, %s)
-ON CONFLICT DO NOTHING;"""
+ON CONFLICT DO NOTHING
+RETURNING job_link;"""
