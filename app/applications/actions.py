ab6e126d63635ea12d7f6f2746617b39fd7739e8
Elad <63946625+eladmines@users.noreply.github.com>
2024-08-09 20:38:01 +0300

    Refactor save_application function (code review).

--- app/applications/actions.py
+++ app/applications/actions.py
@@ -4,41 +4,45 @@ from app.models.job import Job
 from app.utils import save_query_exec,remove_special_chars,delete_query_exec,get_query_exec
 from flask import jsonify
 
-def get_applications(data):
-    data=(data,data)
-    rows=get_query_exec(GET_ALL_APPLICATIONS,data)
-    applications = []
-    for row in rows:
-        title= remove_special_chars(row['job_title'])
-        location=row['job_location']
-        description=remove_special_chars(row['job_description'])
-        qualifications=remove_special_chars("")
-        date=row['job_date']
-        link=row['job_link']
-        company=row['job_company']
-        id=row['job_id']
-        saved=row['saved']
-        applied=row['applicated_date']
-        job = Job(title,location,description,qualifications, date,link,company,id,saved,applied)
-        job = job.__dict__
-        applications.append(job)
+def get_applications(user_id):
+    user_id={'user_id':user_id}
+    rows=get_query_exec(GET_ALL_APPLICATIONS,user_id)
+    
+    applications = [
+        Job(
+            title=remove_special_chars(row['job_title']),
+            location=row['job_location'],
+            description=remove_special_chars(row['job_description']),
+            qualifications=remove_special_chars(""),
+            date=row['job_date'],
+            link=row['job_link'],
+            company=row['job_company'],
+            id=row['job_id'],
+            saved=row['saved'],
+            applied=row['applicated_date']
+        ).__dict__ for row in rows
+    ]
     return applications,None 
 
-def save_application(data):
+def save_application(user_id,job_id):
+    data={'user_id':user_id,'job_id':job_id}
     save_query_exec(SAVE_JOB_APPLICATION,data)
     
 
 def remove_application(data):
+     data={'user_id':data[0],'job_id':data[1]}
      delete_query_exec(DELETE_APPLICATION,data)
 
 def get_applications_processes(user_id,job_id):
-    data=(user_id,job_id)
+    data={'user_id':data[0],'job_id':data[1]}
     rows=get_query_exec(GET_PROCESSES_APPLICATION,data)
     return rows,None 
 
 def remove_process_application(data):
+     data={'id':data}
      delete_query_exec(DELETE_PROCESS_APPLICATION,data)
 
      
 def save_process(data):
+    data={'user_id':data[0],'job_id':data[1]}
     return save_query_exec(SAVE_PROCESS,data)
