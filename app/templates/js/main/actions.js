95594e445297d89f3180854333b8c0ad54fd9f25
Elad <63946625+eladmines@users.noreply.github.com>
2024-08-18 22:19:24 +0300

    Refactor 'initPage', 'initUserData', and 'initProfileData' to use JWT over cookies.

--- app/templates/js/main/actions.js
+++ app/templates/js/main/actions.js
@@ -1,30 +1,50 @@
-import {sendData,almostReady,getUserSession,removeSearchBar} from '../utils.js'
+import {almostReady,getUserSession,removeSearchBar,getUserData} from '../utils.js'
 import {setLineChart} from '../demo/chart-area-demo.js'
 import {Profile} from '../models/profile-info.js'
 
 export async function initPage(){
-    initUserData();
-    initProfileData();
+    var token = getUserSession();
+    var details = await getUserData(token);
+    initUserData(details);
+    initProfileData(details);
     removeSearchBar();
     document.getElementById("generateReport").addEventListener('click',almostReady);
 }
 
 
-function initUserData(){
-    var user = getUserSession();
-    document.getElementById("role").innerText=user.role;
-    document.getElementById("company").innerText=user.company;
-    document.getElementById("experience").innerText=user.experience;
-    document.getElementById("skills").innerText=user.skills;
+async function initUserData(details){
+    if(!details){
+        document.getElementById("role").innerText="Guest"
+        document.getElementById("company").innerText="Unknown";
+        document.getElementById("experience").innerText="Unknown";
+        document.getElementById("skills").innerText="Unknown";
+    } 
+    else{
+        document.getElementById("role").innerText=details["role"];
+        document.getElementById("company").innerText=details["company"];
+        document.getElementById("experience").innerText=details["experience"];
+        document.getElementById("skills").innerText=details["skills"];
+    }
+    
 }
 
-async function initProfileData(){
-    var id = getUserSession().id;
-    var profileData = await sendData('/main',id,'get profile data');
-    var userProfile = new Profile(id,profileData['saved_jobs_count'],profileData['daily_applications_count'],profileData['weekly_applications_count'],profileData['monthly_applications_count'],profileData['weekly_applications_goal'],profileData['array_of_monthly_applications_count']);
-    document.getElementById("savedjobs-counter").innerText=userProfile.getSavedJobsCount();
-    document.getElementById("apply-today").innerText=userProfile.getDailyApplicationsCount();
-    document.getElementById("weekly-goal-percent").innerHTML=userProfile.getWeeklyApplicationsGoal()+'%';
-    document.getElementById("weekly-goal-progress-bar").style.width=userProfile.getWeeklyApplicationsCount()+'%';
-    setLineChart(userProfile.getArrayOfMonthlyApplicationsCount())
+async function initProfileData(details){
+    var userSession = getUserSession()
+    if(!userSession){
+        document.getElementById("savedjobs-counter").innerText="Unknown";
+        document.getElementById("apply-today").innerText="Unknown";
+        document.getElementById("weekly-goal-percent").innerHTML="Unknown";
+        document.getElementById("weekly-goal-progress-bar").style.width="Unknown";
+        setLineChart([[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],[8,0],[9,0],[10,0],[11,0],[12,0]])
+    }
+    else{
+        var userProfile = new Profile(details['id'],details['saved_jobs_count'],details['daily_applications_count'],details['weekly_applications_count'],details['monthly_applications_count'],details['weekly_applications_goal'],details['array_of_monthly_applications_count']);
+        document.getElementById("savedjobs-counter").innerText=userProfile.getSavedJobsCount();
+        document.getElementById("apply-today").innerText=userProfile.getDailyApplicationsCount();
+        document.getElementById("weekly-goal-percent").innerHTML=userProfile.getWeeklyApplicationsGoal()+'%';
+        document.getElementById("weekly-goal-progress-bar").style.width=userProfile.getWeeklyApplicationsCount()+'%';
+        setLineChart(userProfile.getArrayOfMonthlyApplicationsCount())
+        }
 }
+   
+
--- app/templates/js/main/main.js
+++ app/templates/js/main/main.js
@@ -1,6 +1,3 @@
 import { initPage} from './actions.js'
-import { checkUserLogin } from '../utils.js'
 //Entry point
 initPage()
-
-checkUserLogin()
