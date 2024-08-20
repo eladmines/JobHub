01b8cc6cfcbb8b18cf687bc7db9d6af044c8f702
Elad <63946625+eladmines@users.noreply.github.com>
2024-08-20 22:51:06 +0300

    Reassign buttons in order to support guest visitors.

--- app/templates/js/top_bar.js
+++ app/templates/js/top_bar.js
@@ -1,7 +1,32 @@
-import {getCookieValue,getUserSession} from "./utils.js"
+import {getCookieValue,getUserSession, sendData,navigateToPage} from "./utils.js"
 function initTopBar(){
-    var user = getUserSession('user');
-    document.getElementById("name").innerText="Welcome back, " + user.firstname +" "+user.lastname;
+    getNameByToken();
+    assignClickActions();
 }
+
+async function getNameByToken(){
+    var token = getUserSession();
+    var nameValue = document.getElementById("name");
+    if(token){
+        var res = await sendData('/top-bar',token,'')
+        nameValue.innerText="Welcome back, " + res[0]['firstname'] +" "+ res[0]['lastname'];
+    }
+    else{
+        nameValue.innerText = "Login / Register";
+    }
+    
+}
+
+function assignClickActions(){
+    document.addEventListener("DOMContentLoaded", function() {
+        var nameElement = document.getElementById("name");
+        nameElement.onclick = function() {
+            if (nameElement.innerText === "Login / Register") {
+                window.location.href = '/login'; // Navigate to home page if the text is "Login / Register"
+            }
+        };
+    });
+}
+
 //Entry point
 initTopBar()
