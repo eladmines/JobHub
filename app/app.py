cfc13e84d533c81f70d85878e9bc5903a92d4c1c
Elad <63946625+eladmines@users.noreply.github.com>
2024-08-26 13:04:07 +0300

    Register top_bar bp

--- app/app.py
+++ app/app.py
@@ -6,9 +6,11 @@ from app.register.routes import register_bp
 from app.savedjobs.routes import savedjobs_bp
 from app.profile.routes import profile_bp
 from app.applications.routes import applications_bp
-from app.comments.routes import comments_bp
+from app.sending_email.routes import sending_email_bp
 from app.connections.routes import connections_bp
 from app.forgot_password.routes import forgot_password_bp
+from app.top_bar.routes import top_bar_bp
+
 
 app = Flask(__name__,static_folder='templates')
 app.register_blueprint(main_bp)    
@@ -18,6 +20,7 @@ app.register_blueprint(register_bp)
 app.register_blueprint(savedjobs_bp)
 app.register_blueprint(profile_bp)
 app.register_blueprint(applications_bp)
-app.register_blueprint(comments_bp)
+app.register_blueprint(sending_email_bp)
 app.register_blueprint(connections_bp)
 app.register_blueprint(forgot_password_bp)
+app.register_blueprint(top_bar_bp)
