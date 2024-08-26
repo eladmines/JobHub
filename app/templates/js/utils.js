d67be981b683a5d26e33a2d15211dbd427ae7d97
Elad <63946625+eladmines@users.noreply.github.com>
2024-08-26 21:20:23 +0300

    Delete debugging commands.

--- app/templates/js/utils.js
+++ app/templates/js/utils.js
@@ -46,7 +46,7 @@ export function sendData(endpoint, sentData, action) {
         if (response.ok) {
             return response.json();
         } else {
-            console.log("response.json",response.json())
+            console.log(response)
             return false;
         }
     })
