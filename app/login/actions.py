4215410c0129b770a5b639ec1fb65f2578b87ab2
Elad <63946625+eladmines@users.noreply.github.com>
2024-08-26 16:39:20 +0300

    Using logging package for debugging

--- app/login/actions.py
+++ app/login/actions.py
@@ -5,17 +5,22 @@ from sqlalchemy import text
 import jwt
 from flask import jsonify
 from app.config import SECRET_KEY
+import logging
 def authentication(email, password):
     try:
         con = open_connection()
         query = text(USER_AUTHENTICATION)
         result = con.execute(query, {"email": email})
-        print("result",result)
+        logging.basicConfig(filename='app.log', level=logging.DEBUG)
+        logging.debug('This is a debug message with result1: %s', result)
+
         user = result.fetchone()
-        print("user",user)
+        logging.debug('This is a debug message with result2: %s', user)
+
         if user:
             decode_password = user[4]
-            print("decode_password", decode_password)
+            logging.debug('This is a debug message with result3: %s', decode_password)
+
             is_password_correct = check_password(password, decode_password)
             if is_password_correct:
                 token = jwt.encode({ 'user_id': user[0]},SECRET_KEY, algorithm='HS256')
@@ -30,7 +35,7 @@ def authentication(email, password):
 
 def check_password(password: str, hashed: str) -> bool:
     # Check if the provided password matches the hashed password
-    print("inside check_password", password, hashed)
+    logging.debug('This is a debug message with result3: %s %s', password,hashed)
     res = bcrypt.checkpw(password.encode('utf-8'), hashed.encode('utf-8'))
-    print ("res hash", res)
+    logging.debug('This is a debug message with result3: %s', res)
     return res
